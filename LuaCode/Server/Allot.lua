01c5e932e429af89ec58a93dfe0e5a429cf81ec3
夷则 <lsjlkz@gmail.com>
2022-07-25 19:03:40 +0800

    lua的tick

--- LuaCode/Server/Allot.lua
+++ LuaCode/Server/Allot.lua
@@ -11,7 +11,7 @@ __G__AllotIDTable = __G__AllotIDTable or {}
 __G__AllotIDTable.curID = 0
 
 function __G__AllotIDTable.AllotID()
-    __G__AllotIDTable.curID = __G__AllotIDTable.curID + 1
+    __G__AllotIDTable.curID = (__G__AllotIDTable.curID + 1) % 10000
     return cGameServer.Seconds() * 10000 + __G__AllotIDTable.curID
 end
 
